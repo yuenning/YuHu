586d40b60c8f39fa3177b2bddac4d85b1a8adf70
POLLURU MADHU <E0969403@U.NUS.EDU>
2023-05-24 21:03:55 +0800

    amend footer to always be at the bottom of page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,24 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  document.addEventListener("DOMContentLoaded", function() {
+    const footer = document.getElementById("footer");
+  
+    function checkFooterVisibility() {
+      const contentHeight = document.querySelector(".content").offsetHeight;
+      const viewportHeight = window.innerHeight;
+      const scrollPosition = window.pageYOffset;
+  
+      if (contentHeight - viewportHeight <= scrollPosition) {
+        footer.classList.add("show");
+      } else {
+        footer.classList.remove("show");
+      }
+    }
+  
+    window.addEventListener("scroll", checkFooterVisibility);
+  });  
+
   return (
     <footer>
       <footer className={styles["footer"]}>
