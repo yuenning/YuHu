1b1aaca34aa1b60c10d78a964f5b1b8a75ff24bd
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-05-28 01:26:07 +0800

    Edited index.html to fix manifest errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,23 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function () {
     const footer = document.getElementById("footer");
-  
+
     function checkFooterVisibility() {
       const contentHeight = document.querySelector(".content").offsetHeight;
       const viewportHeight = window.innerHeight;
       const scrollPosition = window.pageYOffset;
-  
+
       if (contentHeight - viewportHeight <= scrollPosition) {
         footer.classList.add("show");
       } else {
         footer.classList.remove("show");
       }
     }
-  
+
     window.addEventListener("scroll", checkFooterVisibility);
-  });  
+  });
 
   return (
     <footer className={styles["footer"]}>
