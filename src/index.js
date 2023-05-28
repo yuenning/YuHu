b115f162403eac9f2460df3a24bf85331f51dded
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-05-28 13:54:12 +0800

    Edited index.html to fix errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
-import { createRoot } from "react-dom/client";
+
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase/config"; // Import your Firebase config object
+
+// Initialize Firebase app
+initializeApp(firebaseConfig);
 
 const container = document.getElementById("root");
-if (container) {
-  container.textContent = "container";
-}
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <App />
     </AuthContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
