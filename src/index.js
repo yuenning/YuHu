0f24b78c628d41cf8a520303571d29bcb82e8277
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-05-28 23:08:47 +0800

    Restarting

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase/config"; // Import your Firebase config object
-
 // Initialize Firebase app
-initializeApp(firebaseConfig);
-
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
