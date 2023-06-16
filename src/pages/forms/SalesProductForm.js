d54320a3df86aca2dcb59d79df7acce06e38ec63
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-06-17 00:51:56 +0800

    debug forms and change format

--- src/pages/forms/SalesProductForm.js
+++ src/pages/forms/SalesProductForm.js
@@ -1,3 +1,167 @@
+import { useState } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { FaTimes } from "react-icons/fa";
+import { projectFirestore } from "../../firebase/config";
+
+export default function SalesProductForm({ uid, onSubmit, onDelete }) {
+  const [productId, setProductId] = useState("");
+  const [productName, setProductName] = useState("");
+  const [sellingPrice, setSellingPrice] = useState("");
+  const [quantity, setQuantity] = useState("");
+
+  // Retrieve the user from the authentication context
+  const { user } = useAuthContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form Data:", {
+      productId,
+      productName,
+      sellingPrice,
+      quantity,
+    });
+
+    const productData = {
+      productId,
+      productName,
+      sellingPrice: parseFloat(sellingPrice),
+      quantity: parseInt(quantity),
+    };
+
+    // Save the product data to Firebase
+    projectFirestore
+      .collection(`users/${user.uid}/salesitems`)
+      .add(productData)
+      .then(() => {
+        console.log("Product saved successfully!");
+        console.log(productData);
+        onSubmit(productData);
+        // Reset the form
+        setProductId("");
+        setProductName("");
+        setSellingPrice("");
+        setQuantity("");
+      })
+      .catch((error) => {
+        console.error("Error saving product: ", error);
+      });
+  };
+
+  return (
+    <>
+      <h3 style={{ display: "flex", alignItems: "center" }}>
+        Restock Product Details
+        <button
+          style={{
+            background: "none",
+            color: "#777",
+            border: "2px solid black",
+            padding: "2px",
+            marginLeft: "10px",
+            textAlign: "center",
+            lineHeight: "1",
+            fontSize: "0.9em",
+            cursor: "pointer",
+            width: "30px",
+            height: "30px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          onClick={onDelete}
+        >
+          <FaTimes
+            style={{
+              margin: "0",
+              color: "black",
+              fontSize: "1.2em",
+            }}
+          />
+        </button>
+      </h3>
+      <div style={{ paddingBottom: "20px" }}>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Product Name:</span>
+                <input
+                  name="productName"
+                  type="text"
+                  required
+                  onChange={(e) => setProductName(e.target.value)}
+                  value={productName}
+                />
+              </label>
+            </div>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Product ID:</span>
+                <input
+                  name="productId"
+                  type="text"
+                  required
+                  onChange={(e) => setProductId(e.target.value)}
+                  value={productId}
+                />
+              </label>
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Quantity:</span>
+                <input
+                  name="productQuantity"
+                  type="number"
+                  required
+                  onChange={(e) => setQuantity(e.target.value)}
+                  value={quantity}
+                />
+              </label>
+            </div>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Selling Price:</span>
+                <input
+                  name="sellingPrice"
+                  type="number"
+                  required
+                  onChange={(e) => setSellingPrice(e.target.value)}
+                  value={sellingPrice}
+                />
+              </label>
+            </div>
+          </div>
+          <div style={{ alignSelf: "flex-end", marginTop: "10px" }}>
+            <button
+              type="submit"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "auto",
+                padding: "10px",
+                color: "white",
+                border: "2px solid white",
+                borderRadius: "5px",
+                fontSize: "100%",
+              }}
+            >
+              Save Product
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+/*
 import { useState, useEffect } from "react";
 import { useFirestore } from "../../hooks/useFirestore";
 import { FaTimes } from "react-icons/fa";
@@ -115,3 +279,4 @@ export default function SalesProductForm({ uid, onSubmit, onDelete }) {
     </>
   );
 }
+*/
--- src/pages/forms/SalesTransactionForm.js
+++ src/pages/forms/SalesTransactionForm.js
@@ -1,3 +1,136 @@
+import { useState } from "react";
+import { projectFirestore } from "../../firebase/config";
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+export default function SalesTransactionForm({ uid, onSubmit }) {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [transactionID, setTransactionID] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState("");
+
+  // to add the input into user-segregated firestore
+  const { user } = useAuthContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form Data:", {
+      date,
+      time,
+      transactionAmount,
+      transactionID,
+    });
+
+    const transactionData = {
+      date: new Date(date).toISOString(),
+      time: time,
+      transactionID: transactionID,
+      transactionAmount: parseFloat(transactionAmount),
+    };
+
+    // Save the transaction data to Firebase
+    projectFirestore
+      .collection(`users/${user.uid}/restocks`)
+      .add(transactionData)
+      .then((docRef) => {
+        console.log("Transaction saved successfully with ID:", docRef.id);
+        onSubmit(transactionData);
+        // Reset the form
+        setDate("");
+        setTime("");
+        setTransactionID("");
+        setTransactionAmount("");
+      })
+      .catch((error) => {
+        console.error("Error saving transaction: ", error);
+      });
+  };
+
+  return (
+    <>
+      <h3>Sales Transaction Details</h3>
+      <div style={{ paddingBottom: "20px" }}>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Date:</span>
+                <input
+                  name="date"
+                  type="date"
+                  required
+                  onChange={(e) => setDate(e.target.value)}
+                  value={date}
+                />
+              </label>
+            </div>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Time:</span>
+                <input
+                  name="time"
+                  type="time"
+                  required
+                  onChange={(e) => setTime(e.target.value)}
+                  value={time}
+                />
+              </label>
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Transaction/Invoice ID:</span>
+                <input
+                  name="transactionID"
+                  type="text"
+                  required
+                  onChange={(e) => setTransactionID(e.target.value)}
+                  value={transactionID}
+                />
+              </label>
+            </div>
+            <div style={{ width: "45%" }}>
+              <label>
+                <span>Total Transaction Amount:</span>
+                <input
+                  name="transactionAmount"
+                  type="number"
+                  required
+                  onChange={(e) => setTransactionAmount(e.target.value)}
+                  value={transactionAmount}
+                />
+              </label>
+            </div>
+          </div>
+          <div style={{ alignSelf: "flex-end", marginTop: "10px" }}>
+            <button
+              type="submit"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "auto",
+                padding: "10px",
+                color: "white",
+                border: "2px solid white",
+                borderRadius: "5px",
+                fontSize: "100%",
+              }}
+            >
+              Save Transaction
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+/*
 import { useState, useEffect } from "react";
 import { useFirestore } from "../../hooks/useFirestore";
 import { useAuthContext } from "../../hooks/useAuthContext";
@@ -83,3 +216,4 @@ export default function SalesTransactionForm({ uid }) {
     </>
   );
 }
+*/
