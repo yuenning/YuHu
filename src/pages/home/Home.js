2a8a14c83ad135e9ed838c6fc90258ca6285d2bf
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-06-21 16:03:55 +0800

    edit formatting

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -37,7 +37,6 @@ export default function Home() {
       <div className={styles.metricsContainer}>
         <OverallSalesMetrics />
         <OverallProductAnalysis />
-        <OverallInventoryTurnover />
       </div>
       <div className={styles.listContainer}>
         <MonthlySalesMetrics />
