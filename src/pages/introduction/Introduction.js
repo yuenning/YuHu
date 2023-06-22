15120c8ec0e41b7a5130fa7a0dbca4557d849b34
Ng Yuen Ning <ngyuenning@u.nus.edu>
2023-06-23 00:42:54 +0800

    debug login page, upload introduction gif

--- src/pages/introduction/Introduction.js
+++ src/pages/introduction/Introduction.js
@@ -1,9 +1,12 @@
 import styles from "./Introduction.module.css";
 import React from "react";
+
+// import components
 import dashboardsIcon from "./dashboards_icon.png";
 import trackingIcon from "./tracking_icon.png";
 import orderManagementIcon from "./order_management_icon.png";
 import alertsIcon from "./alerts_icon.png";
+import WarehouseBackground from "./WarehouseBackground.gif";
 
 class InventoryApp extends React.Component {
   componentDidMount() {
@@ -23,7 +26,14 @@ class InventoryApp extends React.Component {
     return (
       <div>
         {/* Landing Page Animation */}
-        <section id="home" className={styles["animation"]}>
+        <section
+          id="home"
+          className={`${styles["animation"]} ${styles["landingSection"]}`}
+        >
+          <div
+            className={styles["landingBackground"]}
+            style={{ backgroundImage: `url(${WarehouseBackground})` }}
+          ></div>
           <div className={styles["landingText"]}>
             <h1>Welcome to Yuhu!!</h1>
             <p>the yoo-hoo to your inventory boo-hoo</p>
@@ -31,25 +41,27 @@ class InventoryApp extends React.Component {
         </section>
 
         {/* About Us Section */}
-        <section id="about" className={styles["about-us"]}>
-          <h1>About Us</h1>
-          <p>
-            At YuHu, we aim to empower small businesses like you with a
-            comprehensive digital solution to transform your stock-taking
-            process and inventory management. Our user-friendly web application
-            streamlines inventory tasks, saving valuable time and effort for
-            small business owners like you. With advanced analytics and
-            automated alerts, we provide valuable insights into your inventory
-            performance, enabling informed decision-making to reduce costs and
-            increase profitability for you. We understand the challenges faced
-            by small businesses in accessing affordable and intuitive inventory
-            management tools, which is why our solution is specifically designed
-            to cater to your unique needs and budgets. At YuHu, we are committed
-            to helping small businesses thrive and succeed by providing them
-            with the tools you need to optimize your inventory management
-            processes.
-          </p>
-        </section>
+        <div className={styles["about-us-box"]}>
+          <section id="about" className={styles["about-us"]}>
+            <h1>About Us</h1>
+            <p>
+              At YuHu, we aim to empower small businesses like you with a
+              comprehensive digital solution to transform your stock-taking
+              process and inventory management. Our user-friendly web
+              application streamlines inventory tasks, saving valuable time and
+              effort for small business owners like you. With advanced analytics
+              and automated alerts, we provide valuable insights into your
+              inventory performance, enabling informed decision-making to reduce
+              costs and increase profitability for you. We understand the
+              challenges faced by small businesses in accessing affordable and
+              intuitive inventory management tools, which is why our solution is
+              specifically designed to cater to your unique needs and budgets.
+              At YuHu, we are committed to helping small businesses thrive and
+              succeed by providing them with the tools you need to optimize your
+              inventory management processes.
+            </p>
+          </section>
+        </div>
 
         {/* Features Section */}
         <section id="features" className={styles["features"]}>
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,3 +1,77 @@
+import { useState, useContext } from "react";
+import { projectAuth } from "../../firebase/config";
+import { AuthContext } from "../../context/AuthContext";
+
+// styles
+import styles from "./Login.module.css";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [formError, setFormError] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setFormError(null);
+
+    // Validate email and password
+    if (email.trim() === "" || password.trim() === "") {
+      setFormError("Please enter both email and password.");
+      return;
+    }
+
+    // Proceed with login
+    try {
+      await projectAuth.signInWithEmailAndPassword(email, password);
+      // Login successful
+      const user = projectAuth.currentUser;
+      dispatch({ type: "LOGIN", payload: user });
+      console.log("Logged in successfully!");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles["login-form"]}>
+      <h2>Welcome back to YuHu!</h2>
+      {formError && (
+        <div>
+          <br></br>
+          <p>{formError}</p>
+        </div>
+      )}
+      {error && (
+        <div>
+          <br></br>
+          <p>Invalid email or password. Please try again.</p>
+        </div>
+      )}
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
+}
+
+/* PREVIOUS CODE WITH HOOK
 import { useState } from "react";
 import { useLogin } from "../../hooks/useLogin";
 
@@ -44,3 +118,4 @@ export default function Login() {
     </form>
   );
 }
+*/
